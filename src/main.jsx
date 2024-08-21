6dda6e6a0a67d9de79398dfac2c2e117bb50a404
diascw <wanessadiascw@gmail.com>
2024-08-21 15:25:24 -0300

    repositorio da loja espaço planejado

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { createRoot } from 'react-dom'; 
 import App from './components/App';
-import './index.css';
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
