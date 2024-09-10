6d5c0e587a58fd700a3482f920d9186893f3249e
Wanessa Dias <130778028+diascw@users.noreply.github.com>
2024-09-10 10:46:43 -0300

    Atualização do numero de Whatsapp Buttons.jsx

--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -4,7 +4,7 @@ import styles from './buttons.module.css';
 function Buttons() {
 
   const links = [
-    { text: 'WhatsApp', url: 'https://w.app/wVEprc' },
+    { text: 'WhatsApp', url: 'https://wa.me/5531992891787?text=Ol%C3%A1!%20Encontrei%20o%20n%C3%BAmero%20da%20empresa%20no%20instagram%20e%20tenho%20interesse%20em%20saber%20mais!' },
     { text: 'Instagram', url: 'https://www.instagram.com/espacoplanejado.oficial/' },
     { text: 'E-mail', url: 'mailto:espacoplanejadomovelaria2009@gmail.com' }
   ];
